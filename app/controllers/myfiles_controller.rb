654f76aad99d55ef6e8812aa3348fc726976483b
kohei <hisashi.kk@gmail.com>
2018-03-30 01:50:37 +0900

    xlsx upload modified

--- app/controllers/myfiles_controller.rb
+++ app/controllers/myfiles_controller.rb
@@ -112,13 +112,15 @@ class MyfilesController < ApplicationController
     elsif file_object.size > 10.megabyte
       result = 'ファイルサイズは10MBまでです。'
     else
-      File.open("public/#{file_name.toutf8}", 'wb') { |f| f.write(file_object.read) }
+      # File.open("public/#{file_name.toutf8}", 'wb') { |f| f.write(file_object.read) }
+      File.open("public/#{file_name}", 'wb') { |f| f.write(file_object.read) }
       result = 'success'
     end
     result
 end
 
   def deletexlsx(file_name)
-    File.unlink 'public/' + file_name.toutf8
+    # File.unlink 'public/' + file_name.toutf8
+    File.unlink 'public/' + file_name
 end
 end
