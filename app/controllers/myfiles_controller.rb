828b308659896d85ea79e204a18d0683b88621df
kohei <hisashi.kk@gmail.com>
2018-03-30 23:46:44 +0900

    modified view

--- app/controllers/myfiles_controller.rb
+++ app/controllers/myfiles_controller.rb
@@ -1,6 +1,6 @@
 class MyfilesController < ApplicationController
   before_action :set_myfile, only: %i[show edit update destroy]
-
+  before_action :authenticate_user!
   # GET /myfiles
   # GET /myfiles.json
   def index
